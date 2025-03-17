59b270a8bf0b8c9b6ffda261d9abc76cbab26e75
Myles Linder <myles.linder@gmail.com>
2025-03-17 12:12:27 -0700

    smoothie fruit api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 
 helpful_links = [
@@ -53,3 +54,10 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(name_on_order + ", your Smoothie is ordered!", icon="✅")
+
+smoothiefroot_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon"
+)
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response, use_container_width=True)
+
