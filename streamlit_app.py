54af1a21fc406d29f2288e708789f4393d13116c
Myles Linder <myles.linder@gmail.com>
2025-03-17 12:30:18 -0700

    search on w pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 
 helpful_links = [
@@ -21,9 +22,12 @@ st.write("The name on your smoothie will be: " + name_on_order)
 cnx = st.connection("snowflake")
 
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+my_dataframe = session.table("smoothies.public.fruit_options").select(
+    col("FRUIT_NAME"), col("SEARCH_ON")
+)
+pd_df = my_dataframe.to_pandas()
+# st.dataframe(data=pd_df, use_container_width=True)
 
-# st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:", my_dataframe, max_selections=5
@@ -33,6 +37,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
+
+        search_on = pd_df.loc[pd_df["FRUIT_NAME"] == fruit_chosen, "SEARCH_ON"].iloc[0]
+        st.write("The search value for ", fruit_chosen, " is ", search_on, ".")
+
         st.subheader(fruit_chosen + " Nutrition Information")
         smoothiefroot_response = requests.get(
             "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
